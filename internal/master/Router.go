a97da57a1d898ad29c8fa76aa4ae61e79117e150
Boldy97 <robin.de.bruin@hotmail.com>
2020-05-10 22:07:11 +0200

    another try

--- internal/master/Router.go
+++ internal/master/Router.go
@@ -23,6 +23,7 @@ func (this *Router) Start(port int) {
 	endpoint := fmt.Sprintf("0.0.0.0:%d", port)
 	address := flag.String("addr", endpoint, "http service address")
 	err := http.ListenAndServe(*address, this.router)
+	log.Infof("Started http listeren on port %s", port)
 	if err != nil {
 		log.Error("Could not start http listener")
 		log.Panic(err)
